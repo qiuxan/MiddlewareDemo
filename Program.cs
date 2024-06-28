cc235d2047bacdfd464cf85c6c3917014a94cb26
qiuxan <qiuxan@gmail.com>
2024-06-29 09:24:59 +1000

    Modify the HTTP request (adding a header) example middleware

--- Program.cs
+++ Program.cs
@@ -12,11 +12,26 @@ var app = builder.Build();
 app.Use(async (context, next) =>
 {
     var logger = app.Services.GetRequiredService<ILogger<Program>>();
-    logger.LogInformation($"Request Host: {context.Request.Host}");
-    logger.LogInformation("My Middleware - Before");
+
+    logger.LogInformation($"ClientName HttpHeader in Middleware 1 {context.Request.Headers["ClientName"]}");
+    logger.LogInformation($"Add a ClientName HttpHeader in Middleware 1");
+
+    context.Request.Headers.TryAdd("ClientName", "MyClient");
+    logger.LogInformation("My Middleware 1 - Before");
     await next(context);
-    logger.LogInformation("My Middleware - After");
-    logger.LogInformation($"Response StatusCode: {context.Response.StatusCode}");
+    logger.LogInformation("My Middleware 1 - After");
+    logger.LogInformation($"Response StatusCode in Middleware 1:{ context.Response.StatusCode}");
+});
+
+app.Use(async (context, next) =>
+{
+    var logger = app.Services.GetRequiredService<ILogger<Program>>();
+
+    logger.LogInformation($"ClientName HttpHeader in Middleware 2 {context.Request.Headers["ClientName"]}");
+    logger.LogInformation("My Middleware 2 - Before");
+    await next(context);
+    logger.LogInformation("My Middleware 2 - After");
+    logger.LogInformation($"Response StatusCode in Middleware 1:{context.Response.StatusCode}");
 });
 
 // Configure the HTTP request pipeline.
